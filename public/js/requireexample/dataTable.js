46f9c55dbd80777ae97f1ecc3ef203023b0f8ddd
PmcFizz <mengchen_0212@foxmail.com>
2016-05-20 17:09:39 +0800

    add echarts modal AND
    add echarts modal

--- public/js/requireexample/dataTable.js
+++ public/js/requireexample/dataTable.js
@@ -1,10 +1,18 @@
 require.config({
-	baseUrl: "http://127.0.0.1:8020/uploadGit",
+	//使用HTTTP 的绝对位置
+	//	baseUrl: "http://127.0.0.1:8020/uploadGit",
+	//	paths: {
+	//		"jquery": "plugins/jquery/jquery-1.12.mini",
+	//		"bootstrap": "plugins/bootstrap-3.3.5-dist/js/bootstrap.min",
+	//		"jquery.dataTables": "plugins/dataTable/js/jquery.dataTables.min",
+	//		"datatables.net": "plugins/dataTable/js/dataTables.bootstrap.min"
+	//	},
+	//使用相对位置
 	paths: {
-		"jquery": "plugins/jquery/jquery-1.12.mini",
-		"bootstrap": "plugins/bootstrap-3.3.5-dist/js/bootstrap.min",
-		"jquery.dataTables": "plugins/dataTable/js/jquery.dataTables.min",
-		"datatables.net": "plugins/dataTable/js/dataTables.bootstrap.min"
+		"jquery": "../../../plugins/jquery/jquery-1.12.mini",
+		"bootstrap": "../../../plugins/bootstrap-3.3.5-dist/js/bootstrap.min",
+		"jquery.dataTables": "../../../plugins/dataTable/js/jquery.dataTables.min",
+		"datatables.net": "../../../plugins/dataTable/js/dataTables.bootstrap.min"
 	},
 	shim: {
 		'bootstrap': {
@@ -23,7 +31,7 @@ require.config({
 require(['bootstrap', 'jquery.dataTables', 'datatables.net', 'jquery'], function() {
 	var dataTable = $("#exampleTable").dataTable({
 		"language": {
-			"url": "../plugins/dataTable/js/Chinese.json"
+			"url": "../../plugins/dataTable/js/Chinese.json"
 		},
 		//"paging": true,
 		//"serverSide": true,
@@ -33,7 +41,7 @@ require(['bootstrap', 'jquery.dataTables', 'datatables.net', 'jquery'], function
 		//"bSort": false,
 		"ajax": {
 			//"url": "http://127.0.0.1:8020/uploadGit/plugins/dataTable/js/objects.txt",
-			"url": "../plugins/dataTable/js/objects.txt",
+			"url": "../../plugins/dataTable/js/objects.txt",
 			"type": "get",
 			"data": function(d) {
 				d.keyword = $.trim($("#searchkey").val());

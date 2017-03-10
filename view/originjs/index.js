16e6e0d1f8261822dd7f3a46ebd7614717005a75
Pang Mengchen <mengchen_0212@foxmail.com>
2017-03-10 09:19:02 +0800

    add some content

---
+++ view/originjs/index.js
@@ -0,0 +1,16 @@
+var inputList=document.getElementsByTagName("input");
+var labelList=document.getElementsByTagName("label");
+var container=document.getElementById("container");
+console.log("inputList:"+inputList);
+console.log("labelList:"+labelList);
+console.log("container:"+container);
+console.log("inputList[0].parentNode"+inputList[0].parentNode);
+console.log("inputList[0].parentNode.innerHTML"+inputList[0].parentNode.innerHTML);
+console.log("inputList[0].outerHTML"+inputList[0].outerHTML);
+console.log("container.childNodes"+container.childNodes);
+console.log("container.childNodes[0]"+container.childNodes[0]);
+console.log("container.firstChild"+container.firstChild);
+console.log("container.lastChild"+container.lastChild);
+console.log("inputList[0].nextSibling"+inputList[0].nextSibling)
+console.log("container.children"+container.children);
+console.log("inputList[0].nextElementSibling"+inputList[0].nextElementSibling)

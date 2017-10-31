b0a049569e9c701e9d0f956bdc1e942f0a6a38e2
FizzPang <mengchen_0212@foxmail.com>
2017-10-31 13:09:02 +0800

    add get_poetry.js

--- view/Web_Crawler/crawler_item/get_poetry.js
+++ view/Web_Crawler/crawler_item/get_poetry.js
@@ -1,15 +1,15 @@
 /**
- *公共的抓包工具 只用于单页面抓取图片
- *2017年8月11日15:31:40
+ * Created by Administrator on 2017/10/31.
+ */
+/**
+ *获取唐诗三百首 保存到数据库
+ *2017年10月31日12:35:48
  */
 let superagent = require("superagent");
-let fs = require("fs");
 let cheerio = require("cheerio");
 let async = require("async");
-let http = require("http");
-let request = require("request");
-
-const downloadFoild = "E:/Web_spiders/";
+let mongoose = require('mongoose');
+let Schema = require('mongoose').Schema;
 
 const setData = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -17,63 +17,53 @@ const setData = {
     'Content-Type': 'text/plain; charset=UTF-8'
 };
 
-let docuTitle = "默认title";
 
-//发起图片下载请求
-let getImgFileBySrc = (src, name, cb) => {
-    fs.exists(downloadFoild + docuTitle, function (res) {
-        if (!res) {
-            fs.mkdirSync(downloadFoild + docuTitle);
-        }
-        request.head(src, function (err) {
-            request(src).pipe(fs.createWriteStream(downloadFoild + docuTitle + "/" + name + ".jpg"));
-        });
-    });
-    cb(null, null);
-};
-
-//并发下载图片保存
-let asyncLoadImg = (arr) => {
-    async.mapLimit(arr, 5, (img, cb) => {
-        getImgFileBySrc(img.imgsrc, img.imgname, cb);
-    }, (err, res) => {
-        if (err) console.log("" + err.message);
-        console.log("抓取完毕,文件保存地址为:__" + downloadFoild + docuTitle);
-    });
-};
+const Poetry = mongoose.model('poetry', new Schema({
+    title  :   String,    //诗的标题
+    author :   String,    //作者
+    href   :   String,    //链接
+    content:   String,    //诗的内容
+    dynasty:   String,    //朝代
+    tag    :   Array      //标签
+}))
+mongoose.connect('mongodb://localhost/net_data', function (err) {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("^_^数据库已连接...请开始你的表演^_^")
+    }
+});
 
 //程序主入口
 let mainFun = (target_url, selector) => {
     superagent.get(target_url)
         .set(setData)
         .end((err, res) => {
-            if (err) {
-                console.log("获取目标地址内容错误____" + err.message);
-                return;
-            }
             let $ = cheerio.load(res.text);
-            docuTitle = $("title").text();
             let selectorArr = $(selector);
             let poetryArr = [];
             for (let i = 0; i < selectorArr.length; i++) {
                 let item = $(selectorArr[i]);
-                let poetryhref = item.attr('href');
-                let poetryame = item.text();
-                poetryArr.push({poetryhref, poetryame});
-            }
-            // console.log(poetryArr);
-            //写入到创建的json文件中
-
-            fs.writeFile(__dirname+'/../data/poetry.json', JSON.stringify({poetryArr}), function (err) {
-                if (err) {
-                    console.log("写入目录树错误。。。" + err.message);
-                } else {
-                    console.log("写入目录树完成！");
+                let href = item.attr('href');
+                let title = item.text();
+                let author=item.parent().text();
+                let tag=['唐朝','唐诗三百首','唐代'];
+                let dynasty='唐朝';
+                if(href.indexOf('http://')!==0){
+                    href='http://so.gushiwen.org'+href;
                 }
-            });
-
-
-            // asyncLoadImg(imgArr);
+                poetryArr.push(href,title);
+                let new_poetry = new Poetry({
+                    title,
+                    href,
+                    author,
+                    dynasty,
+                    tag
+                })
+                new_poetry.save();
+                console.log("已保存到数据库^_^");
+            }
+           // console.log(poetryArr);
         })
 };
 

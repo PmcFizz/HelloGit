0da19f4fbec77e84a982e78d9d83505d3816e2de
FizzPang <mengchen_0212@foxmail.com>
2018-06-09 01:04:20 +0800

    修改我发布的电话

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
-var  gulp =require("gulp");
 var watch =require("gulp-watch");
-var browserSync=require("browser-sync");
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task("default",function(){
-	browserSync.init({
-		server:{
-			baseDir:"./"
-		},
-        port:8050
-	});
-
-	gulp.watch("view/**/**").on("create",browserSync.reload);
-	gulp.watch("index.html").on("change",browserSync.reload);
-	gulp.watch("view/**/**").on("change",browserSync.reload);
-	gulp.watch("public/**/**").on("change",browserSync.reload);
-})
+// Static server
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    // gulp.watch("view/**/**.*").on("create",browserSync.reload);
+	// gulp.watch("index.html").on("change",browserSync.reload);
+	// gulp.watch("view/**/**").on("change",browserSync.reload);
+	// gulp.watch("public/**/**").on("change",browserSync.reload);
+});
